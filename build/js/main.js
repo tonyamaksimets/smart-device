52eecdf970e8c0fd2cd0c5854468ddf227bd0b3d
tonyamaksimets <berg_antonina@mail.ru>
2021-02-09 03:29:52 +0300

    Создает блок преимуществ

--- build/js/main.js
+++ build/js/main.js
@@ -51,3 +51,22 @@
     list.style.width = width + 'px';
   }
 })();
+
+'use strict';
+
+(function () {
+  var body = document.querySelector('.page-body');
+  var anchors = body.querySelectorAll('a[href*="#"]');
+
+  if (anchors) {
+    anchors.forEach(function (anchor) {
+      anchor.addEventListener('click', function (evt) {
+        evt.preventDefault();
+
+        var target = anchor.getAttribute('href').slice(1);
+
+        document.getElementById(target).scrollIntoView({behavior: 'smooth', block: 'start'});
+      });
+    });
+  }
+})();
